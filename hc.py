ebc1a8d682e90a993a85aa57628d5faa9e9e783d
rigaahmeti <riga.ahmeti@student.uni-pr.edu>
2026-04-16 11:12:04 +0200

    Projekti

--- hc.py
+++ hc.py
@@ -4,7 +4,7 @@ from hill_cipher import hill_encrypt, hill_decrypt
 from sct import columnar_encrypt, columnar_decrypt
 
 def main():
-    print("   PROJEKT KRIPTOGRAFIE - DETYRA SEMESTRIALE")
+    print("   PROJEKT - Siguri e te dhenave")
     print("-----------------------------------------------")
     print("\nKy program implementon dy algoritme kriptografike:")
     print("1. Hill Cipher - Enkriptim me matrica")
